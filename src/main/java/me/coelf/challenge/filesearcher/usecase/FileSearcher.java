0fc23e5365f8ae3e9b7a6352c738a9f5acc162b3
Filipe Coelho <fcoelho@fircosoft.com>
2019-04-08 07:54:00 +0200

    refactor constant

--- src/main/java/me/coelf/challenge/filesearcher/usecase/FileSearcher.java
+++ src/main/java/me/coelf/challenge/filesearcher/usecase/FileSearcher.java
@@ -11,9 +11,10 @@ import me.coelf.challenge.filesearcher.domain.Ranker;
 
 public class FileSearcher
 {
-    private final FilesRepository filesRepository;
-    private final FilesIndex      index;
-    private final Ranker          ranker;
+    private static final String          QUIT = ":quit";
+    private final        FilesRepository filesRepository;
+    private final        FilesIndex      index;
+    private final        Ranker          ranker;
 
     public FileSearcher(
             FilesRepository filesRepository,
@@ -45,7 +46,7 @@ public class FileSearcher
     public List<RankedFile> search(String textSearch)
             throws NoMatchesFoundException, ExitException
     {
-        if (":quit".equals(textSearch.trim()))
+        if (QUIT.equals(textSearch.trim()))
             throw new ExitException();
         final Set<File> matchingFiles = index.findMatchingFiles(textSearch);
         return ranker.computeRanking(matchingFiles, textSearch);
